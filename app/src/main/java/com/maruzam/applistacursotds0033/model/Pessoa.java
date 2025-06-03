e05ece984c16b397c5f75bc90d1c7dc3afb67725
MaruzamB <maruzambueno@gmail.com>
2025-06-03 09:07:27 -0300

    App Lista Curso TDS_0033

---
+++ app/src/main/java/com/maruzam/applistacursotds0033/model/Pessoa.java
@@ -0,0 +1,53 @@
+package com.maruzam.applistacursotds0033.model;
+
+public class Pessoa {
+
+    //criar os atributos
+    private String primeiroNome;
+    private String sobrenome;
+    private String cursoDesejado;
+    private String telefoneContato;
+
+    //criar os contrutores
+    public Pessoa() {
+    }
+
+
+    //criar os get and sets
+
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
+}
---
+++ app/src/main/java/com/maruzam/applistacursotds0033/view/MainActivity.java
@@ -0,0 +1,56 @@
+package com.maruzam.applistacursotds0033.view;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.maruzam.applistacursotds0033.R;
+import com.maruzam.applistacursotds0033.model.Pessoa;
+
+public class MainActivity extends AppCompatActivity {
+
+    Pessoa pessoa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        pessoa = new Pessoa();
+
+
+
+        pessoa.setPrimeiroNome("Maruzam");
+        pessoa.setSobrenome("Bueno");
+        pessoa.setCursoDesejado("java");
+        pessoa.setTelefoneContato("99999999");
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
